6a623bb7aa6c06751a9ed8603e1e656e7a80c885
Brask <Dafribdfv@gmail.com>
2023-01-28 02:17:06 -0500

    Version base: Calculadorea

---
+++ src/componentes/Boton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../stylesheets/Boton.css'
+
+function Boton(props){
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=')
+  };
+
+  return(
+    <button 
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`.trimEnd()} onClick={() => props.manejarClic(props.children)}>
+        {props.children} 
+    </button>
+  )
+}
+
+export default Boton;
